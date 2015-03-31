6af9abe5d1bb1426e7d2b0e92c638af71e884061
Justin Gagen <j.gagen@gold.ac.uk>
2015-03-31 15:29:05 +0100

    added stats to plot and multi_plot

--- python/eng_multi_plot_b03.py
+++ python/eng_multi_plot_b03.py
@@ -1,10 +1,11 @@
-# eng_multi_plot_b02.py
+# eng_multi_plot_b03.py
 # Version b01
 # by jmg - j.gagen*AT*gold*DOT*ac*DOT*uk
 # March 13th 2015
 
 # Processes genre files output from 'en_genre.py'
 # Plots frequency distribution of artists over time
+# Writes stats to 'results/versionNumber_eng_multi_plot_stats_data.txt'
 # Writes results to 'results/versionNumber_eng_multi_plot_data.txt'
 # Writes run log to 'logs/versionNumber_eng_multi_plot_log.txt'
 # Plots results and writes PNG to 'graphs/versionNumber_genreName_eng_multi_plot.png'
@@ -13,12 +14,15 @@
 
 # import packages
 import os
+import sys
+import numpy as np
+import scipy.stats as stats
 from datetime import datetime
 from collections import Counter
 import matplotlib
 import matplotlib.pyplot as plt
 
-versionNumber = ("b02")
+versionNumber = ("b03")
 
 # define path to 'genres' subdirectory
 fileNames = os.listdir("genres")
@@ -35,9 +39,15 @@ if not os.path.exists("graphs"):
 if not os.path.exists("results"):
     os.makedirs("results")
 
-# open file for writing log
+# open files for output
 logPath = os.path.join("logs", versionNumber + '_eng_multi_plot_log.txt')
 runLog = open(logPath, 'a')
+
+statsPath = os.path.join("results", versionNumber + '_' + '_eng_multi_plot_stats_data.txt')
+statsResults = open(statsPath, 'a')
+
+resultsPath = os.path.join("results", versionNumber + '_eng_multi_plot_data.txt')
+processedResults = open(resultsPath, 'a')
 
 # Initiate timing of run
 runDate = datetime.now()
@@ -47,10 +57,6 @@ startTime = datetime.now()
 runLog.write ('\n' + 'Multi Genre Plotter | ' + 'Version: ' + versionNumber + '\n' + '\n')
 print ('\n' + 'Multi Genre Plotter | ' + 'Version: ' + versionNumber + ' | Starting' + '\n' +'\n')
 
-# open file for output
-resultsPath = os.path.join("results", versionNumber + '_eng_multi_plot_data.txt')
-processedResults = open(resultsPath, 'a')
-
 for index in range(len(fileNames)):
 
   # look for files in 'genres' subfolder
@@ -58,8 +64,8 @@ for index in range(len(fileNames)):
   genreFile = str(fileNames[index])
   genreName, fileExtension = genreFile.split(".")
 
-  processedResults.write('Plotting graph for the genre: ' + genreName + '\n')
-  print('Plotting graph for the genre: ' + genreName + '\n')
+  processedResults.write('\n' + genreName + '\n')
+  print('\n' + 'Plotting graph and calculating statistics for ' + genreName + '\n')
   
   # define path for graphs
   graphPath = os.path.join("graphs", versionNumber + '_' + genreName + "_eng_multi_plot.png")
@@ -111,7 +117,30 @@ for index in range(len(fileNames)):
   plt.savefig(graphPath, format = 'png')
   plt.clf()
 
-# close file
+  #STATS
+  npData = np.array([(xAxis),(yAxis)], dtype=int)
+  npMedian = str(np.median(npData, axis=1))
+  npMean = npData.mean(axis=1)
+  npMin = npData.min(axis=1)
+  npMax = npData.max(axis=1)
+  npStd = npData.std(axis=1, ddof=1)
+  npVar = npData.var(axis=1)
+  npSkew = stats.skew(npData, axis=1)
+  npKurt = stats.kurtosis(npData, axis=1)
+  npRange = (npMax - npMin)
+
+  print ("Data: " + str(npData))
+  print ("Array shape: " + str(npData.shape))
+  print ("Median: " + str(npMedian))
+  print ("Mean: " + str(npMean))
+  print ("StD: " + str(npStd))
+  print ("Skew: " + str(npSkew))
+  print ("Kurtosis: " + str(npKurt))
+
+  statsResults.write(str(genreName) + ' ^ ' + "Median: " + str(npMedian) + ' ^ ' + "Mean: " + str(npMean) + ' ^ ' + "Min: " + str(npMin) + ' ^ ' + "Max: " + str(npMax) + ' ^ ' + "StD: " + str(npStd) + ' ^ ' + "Var: " + str(npVar) + ' ^ ' + "Skew: " + str(npSkew) + ' ^ ' + "Kurtosis: " + str(npKurt) + ' ^ ' + "Range: " + str(npRange) + '\n')
+
+# close files
+statsResults.close()
 processedResults.close()
 
 # End timing of run
@@ -122,8 +151,9 @@ runLog.write ('\n' + 'Run Information' + '\n' + '\n')
 runLog.write ('Version: ' + versionNumber + '\n')
 runLog.write ('Date of run: {}'.format(runDate) + '\n')
 runLog.write ('Duration of run : {}'.format(endTime - startTime) + '\n')
+runLog.write ('Stats are saved to ../results/versionNumber_eng_muti_plot_stats_data.txt' + '\n')
 runLog.write ('Results are saved to ../results/versionNumber_eng_muti_plot_data.txt' + '\n')
-runLog.write ('Graph is saved to ../graphs/versionNumber_genreName_eng_multi_plot.png' + '\n')
+runLog.write ('Graphs are saved to ../graphs/versionNumber_genreName_eng_multi_plot.png' + '\n')
 runLog.close()
 
 # write to screen
@@ -131,5 +161,6 @@ print ('\n' + 'Run Information' + '\n')
 print ('Version: ' + versionNumber)
 print ('Date of run: {}'.format(runDate))
 print ('Duration of run : {}'.format(endTime - startTime))
+print ('Stats are saved to ../results/versionNumber_eng_muti_plot_stats_data.txt')
 print ('Results are saved to ../results/versionNumber_eng_multi_plot_data.txt')
-print ('Graph is saved to ../graphs/versionNumber_genreName_eng_multi_plot.png')
+print ('Graphs are saved to ../graphs/versionNumber_genreName_eng_multi_plot.png')
