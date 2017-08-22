f20c108656e71aa5d484a511f656c5d6e8003815
pha5echange <jmg@phasechange.co.uk>
2017-08-22 16:48:11 +0100

    Updated process_firsts and process_clusters

---
+++ python/eng_process_clusters_b05.py
@@ -0,0 +1,136 @@
+# eng_process_clusters_b05.py
+# Version b05
+# by jmg - j.gagen*AT*gold*DOT*ac*DOT*uk
+# Aug 22nd 2017
+
+# Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
+# Source code at: https://github.com/pha5echange/eng-tools
+
+# Reads results from output of 'eng_cluster.py'
+# Processes file 'data/first_cluster.txt'
+# Offers choice of full-range X-axis, or 1895-2017
+# Writes results to 'results/eng_process_clusters_versionNumber.txt'
+# Writes run log to 'logs/eng_process_clusters_versionNumber_log.txt'
+# Plots results and writes EPS to 'graphs/eng_process_clusters_versionNumber_plot.eps'
+
+# Run AFTER 'eng_cluster.py'
+
+# PLOTS BARS
+
+# import packages
+import os
+from datetime import datetime
+from collections import Counter
+import matplotlib
+import matplotlib.pyplot as plt
+
+versionNumber = ("b05")
+
+# create 'logs' subdirectory if necessary
+if not os.path.exists("logs"):
+		os.makedirs("logs")
+
+# create 'graphs' subdirectory if necessary
+if not os.path.exists("graphs"):
+		os.makedirs("graphs")
+
+# create 'results' subdirectory if necessary
+if not os.path.exists("results"):
+		os.makedirs("results")
+
+# open file for writing log
+logPath = os.path.join("logs", 'eng_process_clusters_' + versionNumber + '_log.txt')
+runLog = open(logPath, 'a')
+
+# define path for graphs
+graphPath = os.path.join("graphs", 'eng_process_clusters_' + versionNumber + "_plot.eps")
+
+# open file for data output
+resultsPath = os.path.join("results", 'eng_process_clusters_' + versionNumber + '.txt')
+processedResults = open(resultsPath, 'a')
+
+# Initiate timing of run
+runDate = datetime.now()
+startTime = datetime.now()
+
+# ..and begin..
+runLog.write ('\n' + 'Genre Data Results Processor | ' + 'Version: ' + versionNumber + '\n' + '\n')
+print ('\n' + 'Genre Data Results Processor | ' + 'Version: ' + versionNumber + ' | Starting' + '\n')
+
+print
+xAxisRange = int(input ("Enter 0 here for full-range X-Axis, or 1 for 1895-2017 range: "))
+print
+
+# look for file in 'data' subfolder
+pathname = os.path.join("data", 'first_cluster.txt')
+dataInput = open(pathname, "r")
+	
+# define list to store the start dates
+clusters = []
+
+# read lines from the file
+for line in dataInput:
+
+	# split line and append 'clusters' with start date values
+	genre, clusterDate, newLine = line.split(",")
+	clusters.append(int(clusterDate))
+
+# close input file
+dataInput.close()
+
+# count and print instances of clusters
+countedClusters = Counter(clusters)
+
+xAxis = []
+yAxis = []
+
+for key, value in sorted(countedClusters.iteritems()):
+	 processedResults.write(str(key) + ',' + str(value) + '\n')
+	 xAxis.append(key)
+	 yAxis.append(value)
+
+# set axes values (N.B. x parameters can be altered to limit time axis)
+if xAxisRange == 0:
+	x_low = (min(xAxis) - 10)
+	x_high = (max(xAxis) + 10)
+else:
+	x_low = (1895)
+	x_high = (2017)
+
+y_low = 0
+y_high = (max(yAxis) + 5)
+
+# plot graph
+width = 1
+plt.bar(xAxis, yAxis, width, color='blue')
+
+# label, plot and save image of graph
+plt.grid(zorder=0)
+plt.xlabel('Year of Inception', fontsize=14)
+plt.ylabel('Number of Genres', fontsize=14)
+plt.xlim(x_low, x_high)
+plt.ylim(y_low, y_high)
+plt.savefig(graphPath, format = 'eps')
+
+# close files
+processedResults.close()
+
+# End timing of run
+endTime = datetime.now()
+
+# write to log
+runLog.write ('\n' + 'Run Information' + '\n' + '\n')
+runLog.write ('Version: ' + versionNumber + '\n')
+runLog.write ('Date of run: {}'.format(runDate) + '\n')
+runLog.write ('Duration of run : {}'.format(endTime - startTime) + '\n')
+runLog.write ('Results are saved to ../results/eng_process_clusters_versionNumber.txt' + '\n')
+runLog.write ('Graph is saved to ../graphs/eng_process_clusters_versionNumber_plot.eps' + '\n')
+runLog.close()
+
+# write to screen
+print ('\n' + 'Run Information' + '\n')
+print ('Version: ' + versionNumber)
+print ('Date of run: {}'.format(runDate))
+print ('Duration of run : {}'.format(endTime - startTime))
+print ('Results are saved to ../results/eng_process_clusters_versionNumber.txt')
+print ('Graph is saved to ../graphs/eng_process_clusters_versionNumber_plot.eps')
--- python/eng_process_firsts_b06.py
+++ python/eng_process_firsts_b06.py
@@ -1,16 +1,17 @@
-# eng_process_clusters_b04.py
-# Version b04
+# eng_process_firsts_b06.py
+# Version b06
 # by jmg - j.gagen*AT*gold*DOT*ac*DOT*uk
-# January 14th 2016
+# Aug 22nd 2017
 
 # Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
 # Source code at: https://github.com/pha5echange/eng-tools
 
-# Reads results from output of 'eng_cluster.py'
-# Processes file 'data/first_cluster.txt'
-# Writes results to 'results/eng_process_clusters_versionNumber.txt'
-# Writes run log to 'logs/eng_process_clusters_versionNumber_log.txt'
-# Plots results and writes EPS to 'graphs/eng_process_clusters_versionNumber_plot.eps'
+# Reads results from output of 'eng_first.py'
+# Processes file 'data/first_instances.txt'
+# Offers choice of full-range X-axis, or 1895-2017
+# Writes results to 'results/eng_process_firsts_versionNumber.txt'
+# Writes run log to 'logs/eng_process_firsts_versionNumber_log.txt'
+# Plots results and writes EPS to 'graphs/eng_process_firsts_versionNumber_plot.eps'
 
 # Run AFTER 'eng_cluster.py'
 
@@ -23,7 +24,7 @@ from collections import Counter
 import matplotlib
 import matplotlib.pyplot as plt
 
-versionNumber = ("b04")
+versionNumber = ("b06")
 
 # create 'logs' subdirectory if necessary
 if not os.path.exists("logs"):
@@ -38,14 +39,14 @@ if not os.path.exists("results"):
     os.makedirs("results")
 
 # open file for writing log
-logPath = os.path.join("logs", 'eng_process_clusters_' + versionNumber + '_log.txt')
+logPath = os.path.join("logs", 'eng_process_firsts_' + versionNumber + '_log.txt')
 runLog = open(logPath, 'a')
 
 # define path for graphs
-graphPath = os.path.join("graphs", 'eng_process_clusters_' + versionNumber + "_plot.eps")
+graphPath = os.path.join("graphs", 'eng_process_firsts_' + versionNumber + "_plot.eps")
 
 # open file for data output
-resultsPath = os.path.join("results", 'eng_process_clusters_' + versionNumber + '.txt')
+resultsPath = os.path.join("results", 'eng_process_firsts_' + versionNumber + '.txt')
 processedResults = open(resultsPath, 'a')
 
 # Initiate timing of run
@@ -53,42 +54,49 @@ runDate = datetime.now()
 startTime = datetime.now()
 
 # ..and begin..
-runLog.write ('\n' + 'Genre Data Results Processor | ' + 'Version: ' + versionNumber + '\n' + '\n')
-print ('\n' + 'Genre Data Results Processor | ' + 'Version: ' + versionNumber + ' | Starting' + '\n')
+runLog.write ('\n' + 'Genre Data First Instances Results Processor | ' + 'Version: ' + versionNumber + '\n' + '\n')
+print ('\n' + 'Genre Data First Instances Results Processor | ' + 'Version: ' + versionNumber + ' | Starting' + '\n')
+
+print
+xAxisRange = int(input ("Enter 0 here for full-range X-Axis, or 1 for 1895-2017 range: "))
+print
 
 # look for file in 'data' subfolder
-pathname = os.path.join("data", 'first_cluster.txt')
+pathname = os.path.join("data", 'first_instances.txt')
 dataInput = open(pathname, "r")
 	
 # define list to store the start dates
-clusters = []
+instances = []
 
 # read lines from the file
 for line in dataInput:
 
-	# split line and append 'clusters' with start date values
-	genre, clusterDate, newLine = line.split(",")
-	clusters.append(int(clusterDate))
+	# split line and append 'instances' with start date values
+	genre, firstInstance, newLine = line.split(",")
+	instances.append(int(firstInstance))
 
 # close input file
 dataInput.close()
 
 # count and print instances of clusters
-countedClusters = Counter(clusters)
+countedInstances = Counter(instances)
 
 xAxis = []
 yAxis = []
 
-for key, value in sorted(countedClusters.iteritems()):
+for key, value in sorted(countedInstances.iteritems()):
    processedResults.write(str(key) + ',' + str(value) + '\n')
    xAxis.append(key)
    yAxis.append(value)
 
 # set axes values (N.B. x parameters can be altered to limit time axis)
-#x_low = (min(xAxis) - 10)
-x_low = (1895)
-#x_high = (max(xAxis) + 10)
-x_high = (2017)
+if xAxisRange == 0:
+  x_low = (min(xAxis) - 10)
+  x_high = (max(xAxis) + 10)
+else:
+  x_low = (1895)
+  x_high = (2017)
+
 y_low = 0
 y_high = (max(yAxis) + 5)
 
@@ -98,13 +106,13 @@ plt.bar(xAxis, yAxis, width, color='blue')
 
 # label, plot and save image of graph
 plt.grid(zorder=0)
-plt.xlabel('Year of Inception', fontsize=14)
+plt.xlabel('Year of Artist First Instance', fontsize=14)
 plt.ylabel('Number of Genres', fontsize=14)
 plt.xlim(x_low, x_high)
 plt.ylim(y_low, y_high)
 plt.savefig(graphPath, format = 'eps')
 
-# close files
+# close file
 processedResults.close()
 
 # End timing of run
@@ -115,14 +123,12 @@ runLog.write ('\n' + 'Run Information' + '\n' + '\n')
 runLog.write ('Version: ' + versionNumber + '\n')
 runLog.write ('Date of run: {}'.format(runDate) + '\n')
 runLog.write ('Duration of run : {}'.format(endTime - startTime) + '\n')
-runLog.write ('Results are saved to ../results/eng_process_clusters_versionNumber.txt' + '\n')
-runLog.write ('Graph is saved to ../graphs/eng_process_clusters_versionNumber_plot.eps' + '\n')
+runLog.write('Graph is saved to ../graphs/eng_process_firsts_versionNumber_plot.eps' + '\n')
 runLog.close()
 
 # write to screen
 print ('\n' + 'Run Information' + '\n')
 print ('Version: ' + versionNumber)
 print ('Date of run: {}'.format(runDate))
-print ('Duration of run : {}'.format(endTime - startTime))
-print ('Results are saved to ../results/eng_process_clusters_versionNumber.txt')
-print ('Graph is saved to ../graphs/eng_process_clusters_versionNumber_plot.eps')
+print('Duration of run : {}'.format(endTime - startTime))
+print('Graph is saved to ../graphs/eng_process_firsts_versionNumber_plot.eps')
